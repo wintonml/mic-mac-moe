9b37cf5c73d4eb18738ae905fa585707a7aacea6
wintonml <wintonml@gmail.com>
2024-07-27 00:56:21 +0200

    Create board using Square component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-function Square() {
-  return <button className="square">1</button>;
+// value is a prop that gets it's value from the parent component that calls this function
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 // Fragments <></> are used to return a group of JSX elements in a return statement
@@ -7,19 +8,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
