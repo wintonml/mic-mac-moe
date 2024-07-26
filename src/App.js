a2741b666ec2c53870f68a0e695937e44bd39134
wintonml <wintonml@gmail.com>
2024-07-27 00:33:58 +0200

    Display 9 squares

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+// Fragments <></> are used to return a group of JSX elements in a return statement
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+    </>
+  );
 }
