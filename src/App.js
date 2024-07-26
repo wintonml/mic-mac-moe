82c277000eee2ec52d82d3258ca5718558200138
wintonml <wintonml@gmail.com>
2024-07-27 00:47:38 +0200

    Create Square function
    This reduces code duplication and hardcoding values

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+function Square() {
+  return <button className="square">1</button>;
+}
+
 // Fragments <></> are used to return a group of JSX elements in a return statement
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
