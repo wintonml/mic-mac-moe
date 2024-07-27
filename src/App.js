e229e98757e1c677e5cfe06d57f3208ca0eb4646
wintonml <wintonml@gmail.com>
2024-07-27 23:02:47 +0200

    Use onClick event to populate board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 // value is a prop that gets it's value from the parent component that calls this function
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 // Fragments <></> are used to return a group of JSX elements in a return statement
@@ -8,19 +19,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
