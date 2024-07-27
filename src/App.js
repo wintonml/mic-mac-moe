e4c37e871f5f35e11d8b5ed64ef847f877b5c9b3
wintonml <wintonml@gmail.com>
2024-07-27 23:11:31 +0200

    Create foundation for board to keep track of the state of the squares

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import { useState } from "react";
 
 // value is a prop that gets it's value from the parent component that calls this function
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 // Fragments <></> are used to return a group of JSX elements in a return statement
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
