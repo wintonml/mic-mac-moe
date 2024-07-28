574f08ada7fcdd94bd2b81212ddd20c3850d2380
wintonml <wintonml@gmail.com>
2024-07-28 08:18:31 +0200

    Add alternating turns

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ function Square({ value, onSquareClick }) {
 
 // Fragments <></> are used to return a group of JSX elements in a return statement
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
